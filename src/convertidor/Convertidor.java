61be445891823b5122a4fc8ef8c8e58ebb352909
Stiven Trujillo <ico1293@gmail.com>
2015-09-18 16:05:53 +0800

    esto es el commit inicial

---
+++ src/convertidor/Convertidor.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package convertidor;
+
+/**
+ *
+ * @author Stiven
+ */
+public class Convertidor {
+    //VARIABLES GLOBALES (NATIVAS)
+    double cantidad;
+    double respuesta;
+    double litros2Galones;
+    double metros2Centimetros;
+    double horas2Segundos;
+    //CONSTRUCTORES
+    //VALORES POR DEFECTO
+    public Convertidor()
+    {
+        cantidad = 0;
+        respuesta = 0;
+        //UN LITRO EQUIVALE A 0.264 GALONES
+        litros2Galones = 0.264;
+        //UN METRO EQUIVALE A 100 CM
+        metros2Centimetros = 100;
+        //UNA HORA EQUIVALE A 3600 SEGUNDOS
+        horas2Segundos = 3600;
+    }
+    //VALORES ASIGNADOS NATIVOS( OSEA AL LLAMAR ESTE CONSTRUCTOR O METODO CONVERTIDOR(NUMERO) ASIGNARA
+    //NUMERO A LA VATIABLE NATIVA DE LA CLASE )
+    public  Convertidor(double cantidad)
+    {
+        //EL THIS ES PARA DIFERENCIAR LA VARIABLE NATIVA DE LA VARIABLE PROPIA DEL METODO
+        this.cantidad = cantidad;
+        litros2Galones = 0.264;
+        metros2Centimetros = 100;
+        horas2Segundos = 3600;
+        respuesta = 0;
+    }
+    //METODOS
+    public double MostrarCantidad()
+    {
+        return cantidad;
+    }
+    public double Calcular()
+    {
+        respuesta = cantidad * metros2Centimetros;
+        return respuesta;
+    }
+    
+    
+}
---
+++ src/convertidor/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package convertidor;
+import convertidor.Convertidor;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Stiven
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        double cantidad = Double.parseDouble(JOptionPane.showInputDialog(null, "ingrese cantidad"));
+        //SE CREA UN OBJETO DE LA SIGUIENTE FORMA: NOMBRE DE LA CLASE-NOMBRE DEL OBJETO-NEW-NOMBRE DEL CONSTRUCTOR
+        Convertidor litros2Galones = new Convertidor(cantidad);
+        
+        
+        //IMPRIMIR RESPUESTA (LLAMAMOS AL METODO DE LA FORMA: NOMBRE DEL OBJETO - . - METODO DE LA CLASE
+        System.out.println(litros2Galones.MostrarCantidad() + " Metros son " + litros2Galones.Calcular() + " CentiMetros");
+ 
+    }
+    
+}
