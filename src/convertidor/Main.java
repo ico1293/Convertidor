9564eeb369dd894dc803241b2d39b7b3e9d9b6e6
Stiven Trujillo <ico1293@gmail.com>
2015-09-18 15:24:54 +0800

    Borrados algunos comentarios

--- src/convertidor/Main.java
+++ src/convertidor/Main.java
@@ -22,24 +22,6 @@ public class Main {
         //INICIAR INTERFAZ
         iniciarInterfaz();
     
-        /*
-         int eleccion = Integer.parseInt(JOptionPane.showInputDialog("1.Metros To Centimetros\n2.Litros To Galones\n3.Horas to Segundos"));
-         double cantidad = Double.parseDouble(JOptionPane.showInputDialog("Ingrese cantidad"));
-         //SE CREA UN OBJETO DE LA SIGUIENTE FORMA: NOMBRE DE LA CLASE-NOMBRE DEL OBJETO-NEW-NOMBRE DEL CONSTRUCTOR
-         Convertidor convertidor = new Convertidor(cantidad);
-        
-         switch (eleccion)
-         {
-         case 1: System.out.println(convertidor.MostrarCantidad() + " Metros son " + convertidor.CalcularMetrosToCentimetros() + " CentiMetros");
-         break;
-         case 2: System.out.println(convertidor.MostrarCantidad() + " Litros son " + convertidor.CalcularLitrosToGalones() + " Galones");
-         break;
-         case 3: System.out.println(convertidor.MostrarCantidad() + " Horas son " + convertidor.CalcularHorasToSegundos() + " Segundos");
-         break;
-         }
-         //IMPRIMIR RESPUESTA (LLAMAMOS AL METODO DE LA FORMA: NOMBRE DEL OBJETO - . - METODO DE LA CLASE
-         System.out.println(convertidor.MostrarCantidad() + " Metros son " + convertidor.Calcular() + " CentiMetros");
-         */
     }
 
     public static void iniciarInterfaz() {
